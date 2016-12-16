cb1463e99d487996a751dac5dbba5adf8de4b0eb
Leo Meyer <leo@leomeyer.de>
2016-12-16 13:53:16 +0100

    Make POCO for i386 silent

--- code/c/libraries/POCO_patches/build_poco_i386.sh
+++ code/c/libraries/POCO_patches/build_poco_i386.sh
@@ -6,4 +6,4 @@ export ARCHFLAGS="-arch i386"
 ./configure --config=Linux_i386 --no-samples --no-tests --omit=CppUnit,CppUnit/WinTestRunner,Crypto,NetSSL_OpenSSL,Data,Data/SQLite,Data/ODBCData/MySQL,MongoDB,PageCompiler,PageCompiler/File2Page
 
 # build
-make
+make -s
