67bf446e12771a58f8a55de7a86238643d444faa
Leo Meyer <leo@leomeyer.de>
2016-12-16 09:46:18 +0100

    Fix i386 build script

--- code/c/libraries/POCO_patches/build_poco_i386.sh
+++ code/c/libraries/POCO_patches/build_poco_i386.sh
@@ -3,7 +3,7 @@ export POCO_TARGET_OSARCH="i386"
 export ARCHFLAGS="-arch i386"
 
 # configure
-./configure --config=RaspberryPi --no-samples --no-tests --omit=CppUnit,CppUnit/WinTestRunner,Crypto,NetSSL_OpenSSL,Data,Data/SQLite,Data/ODBCData/MySQL,MongoDB,PageCompiler,PageCompiler/File2Page
+./configure --config=Linux --no-samples --no-tests --omit=CppUnit,CppUnit/WinTestRunner,Crypto,NetSSL_OpenSSL,Data,Data/SQLite,Data/ODBCData/MySQL,MongoDB,PageCompiler,PageCompiler/File2Page
 
 # build
 make -s
