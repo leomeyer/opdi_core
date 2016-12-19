dcb3b477afcf7c74cfa6935d63de66f223a0dc6a
Leo Meyer <leo@leomeyer.de>
2016-12-19 16:13:10 +0100

    Improve POCO builds

--- code/c/libraries/POCO_patches/build_poco.sh
+++ code/c/libraries/POCO_patches/build_poco.sh
@@ -1,6 +1,6 @@
 
 # configure
-./configure --no-samples --no-tests --omit=CppUnit,CppUnit/WinTestRunner,Crypto,NetSSL_OpenSSL,Data,Data/SQLite,Data/ODBCData/MySQL,MongoDB,PageCompiler,PageCompiler/File2Page
+./configure --no-samples --no-tests --minimal
 
 # build
 make -s
--- code/c/libraries/POCO_patches/build_poco_rpi.sh
+++ code/c/libraries/POCO_patches/build_poco_rpi.sh
@@ -1,6 +1,6 @@
 
 # configure
-./configure --config=RaspberryPi --no-samples --no-tests --omit=CppUnit,CppUnit/WinTestRunner,Crypto,NetSSL_OpenSSL,Data,Data/SQLite,Data/ODBCData/MySQL,MongoDB,PageCompiler,PageCompiler/File2Page
+./configure --config=RaspberryPi --no-samples --no-tests --minimal
 
 # build
 make -s
